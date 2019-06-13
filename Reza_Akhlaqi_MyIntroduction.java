f498818e0f8c2caed1318f37b34b96c1183d163b
Reza Akhlaqi <36783229+rzjrh@users.noreply.github.com>
2019-06-13 00:48:26 -0500

    Add files via upload

---
+++ Reza_Akhlaqi_MyIntroduction.java
@@ -0,0 +1,17 @@
+/*
+ This is my first Java program
+Introduce myself 
+Author: Reza Akhlaqi   6-13-19
+ */
+
+//here we use the System.out.println
+//The first letter in System is capital
+public class Reza_Akhlaqi_MyIntroduction {
+    public static void main(String[] args) {
+System.out.println("My Name is Reza Akhlaqi");
+System.out.println("I am 34 years old");
+System.out.println("My current hometown is Richardson Texas");
+
+    }
+    
+}
